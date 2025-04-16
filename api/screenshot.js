a65c317ca6f9ec314bdf0eaeaf33540b0936b437
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2025-04-16 14:41:45 -0600

    Just init browser

--- api/screenshot.js
+++ api/screenshot.js
@@ -1,4 +1,4 @@
-import { initBrowser } from "./_browser.js";
+import { initBrowser } from ".../lib/browser.js";
 import { screenshotPage } from "../lib/core.js";
 
 export default async function handler(req, res) {
@@ -8,28 +8,32 @@ export default async function handler(req, res) {
     return res.status(400).json({ error: "URL is required" });
   }
 
-  if (!browser) {
-    await initBrowser();
-    let retries = 0;
-    while (!browser && retries < 3) {
-      await new Promise((resolve) => setTimeout(resolve, 2000));
-      if (!browser && !isInitializing) {
-        await initBrowser();
-      }
-      retries++;
-    }
+  let browser = initBrowser();
 
-    if (!browser) {
-      throw new Error("Could not initialize browser after multiple attempts");
-    }
-  }
+  res.status(200).json({ url: url });
 
-  try {
-    const image = await screenshotPage(browser, url, timeout);
-    res.set("Content-Type", "image/jpeg");
-    res.status(200).send(image);
-  } catch (error) {
-    console.error("Error rendering page:", error);
-    res.status(500).json({ error: error.message });
-  }
+  // if (!browser) {
+  //   await initBrowser();
+  //   let retries = 0;
+  //   while (!browser && retries < 3) {
+  //     await new Promise((resolve) => setTimeout(resolve, 2000));
+  //     if (!browser && !isInitializing) {
+  //       await initBrowser();
+  //     }
+  //     retries++;
+  //   }
+
+  //   if (!browser) {
+  //     throw new Error("Could not initialize browser after multiple attempts");
+  //   }
+  // }
+
+  // try {
+  //   const image = await screenshotPage(browser, url, timeout);
+  //   res.set("Content-Type", "image/jpeg");
+  //   res.status(200).send(image);
+  // } catch (error) {
+  //   console.error("Error rendering page:", error);
+  //   res.status(500).json({ error: error.message });
+  // }
 }
