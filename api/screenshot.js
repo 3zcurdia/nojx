bae3daf2595bdd768e7cb90d89ea86650056c002
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2025-04-16 16:47:28 -0600

    Just fetch browser

--- api/screenshot.js
+++ api/screenshot.js
@@ -1,5 +1,5 @@
-import { initBrowser } from ".../lib/browser.js";
-import { screenshotPage } from "../lib/core.js";
+import { initBrowser } from "../lib/browser.js";
+// import { screenshotPage } from "../lib/core.js";
 
 export default async function handler(req, res) {
   const { url, timeout = 15000 } = req.body;
@@ -11,29 +11,29 @@ export default async function handler(req, res) {
   let browser = initBrowser();
 
   res.status(200).json({ url: url });
-
-  // if (!browser) {
-  //   await initBrowser();
-  //   let retries = 0;
-  //   while (!browser && retries < 3) {
-  //     await new Promise((resolve) => setTimeout(resolve, 2000));
-  //     if (!browser && !isInitializing) {
-  //       await initBrowser();
-  //     }
-  //     retries++;
-  //   }
-
-  //   if (!browser) {
-  //     throw new Error("Could not initialize browser after multiple attempts");
-  //   }
-  // }
-
-  // try {
-  //   const image = await screenshotPage(browser, url, timeout);
-  //   res.set("Content-Type", "image/jpeg");
-  //   res.status(200).send(image);
-  // } catch (error) {
-  //   console.error("Error rendering page:", error);
-  //   res.status(500).json({ error: error.message });
-  // }
 }
+
+// if (!browser) {
+//   await initBrowser();
+//   let retries = 0;
+//   while (!browser && retries < 3) {
+//     await new Promise((resolve) => setTimeout(resolve, 2000));
+//     if (!browser && !isInitializing) {
+//       await initBrowser();
+//     }
+//     retries++;
+//   }
+
+//   if (!browser) {
+//     throw new Error("Could not initialize browser after multiple attempts");
+//   }
+// }
+
+// try {
+//   const image = await screenshotPage(browser, url, timeout);
+//   res.set("Content-Type", "image/jpeg");
+//   res.status(200).send(image);
+// } catch (error) {
+//   console.error("Error rendering page:", error);
+//   res.status(500).json({ error: error.message });
+// }
