49370554008361f2bdae43c1b8dce2b95ec3925f
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2025-04-16 14:27:54 -0600

    Implement up and screenshot pages

--- api/fetch.js
+++ api/fetch.js
@@ -1,3 +1,8 @@
 export default async function handler(req, res) {
-  res.status(200).json({});
+  const { url, timeout = 15000 } = req.body;
+
+  if (!url) {
+    return res.status(400).json({ error: "URL is required" });
+  }
+  res.status(200).json({ url: url });
 }
